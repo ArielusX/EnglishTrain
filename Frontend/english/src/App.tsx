e8a42048e8f552e3dabbc209f8bf894e2c27fde6
ArielusX <llugo.vera.ll@gmail.com>
2025-10-10 22:16:37 +0200

    feat: Hero design and some cards

--- Frontend/english/src/App.tsx
+++ Frontend/english/src/App.tsx
@@ -2,32 +2,38 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import TemplateCard from './components/TemplateCard/TemplateCard'
+import Hero from './components/Hero/Hero'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+             <Hero />
+
+<div className='flex'>   <TemplateCard
+  title={"Grammar"}
+  description={"Ejercicios de grammas"}
+  category={"English"}
+  exercises={6}
+  onGenerate={() => console.log("Generar PDF")}
+ />   <TemplateCard
+  title={"Grammar"}
+  description={"Ejercicios de grammas"}
+  category={"English"}
+  exercises={6}
+  onGenerate={() => console.log("Generar PDF")}
+ />   <TemplateCard
+  title={"Grammar"}
+  description={"Ejercicios de grammas"}
+  category={"English"}
+  exercises={6}
+  onGenerate={() => console.log("Generar PDF")}
+ /></div>
+   
+
     </>
   )
 }
